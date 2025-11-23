8ea33439410e8b258b1a8f16c003328f2a0ba14b
Victor Mussoline <victor8augusto010604@hotmail.com>
2025-11-23 18:51:09 -0300

    Versão Final

--- meuScriptOriginal.js
+++ meuScriptOriginal.js
@@ -1,1 +1,35 @@
-console.log(alert)
+let cardContainer = document.querySelector(".card-container");
+let dados = [];
+
+async function carregarDados() {
+    let resposta = await fetch("data.json");
+    dados = await resposta.json();
+    renderizarCards(dados);
+}
+
+carregarDados();
+
+function iniciarBusca() {
+    let busca = document.querySelector('input[type="text"]').value.toLowerCase();
+    let dadosFiltrados = dados.filter(dado => dado.nome.toLowerCase().includes(busca));
+    renderizarCards(dadosFiltrados);
+}
+
+function renderizarCards(dados) {
+    cardContainer.innerHTML = ""
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <div class="card-info">
+            <h2>${dado.nome}</h2>
+            <p>${dado.ano}</p>
+            <p>${dado.plataforma}</p>
+            <a href="${dado.link}" target="_blank">Saiba Mais</a>
+        </div>
+        ${dado.imagem ? `<img src="${dado.imagem}" class="card-image" alt="Imagem do jogo ${dado.nome}">` : ''}
+        `
+
+        cardContainer.appendChild(article);
+    }
+}
--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-let cardContainer = document.querySelector(".card-container");
-let dados = [];
-
-async function iniciarBusca() {
-    let resposta = await fetch("data.json");
-    dados = await resposta.json();
-    renderizarCards(dados);
-}
-
-function renderizarCards(dados) {
-    for (let dado of dados) {
-        let article = document.createElement("article");
-        article.classList.add("card");
-        article.innerHTML = `
-        <h2>${dado.nome}</h2>
-        <p>${dado.ano}</p>
-        <p>${dado.plataforma}</p>
-        <a href="${dado.link}" target="_blank">Saiba Mais</a>
-        `
-
-        cardContainer.appendChild(article);
-    }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const overlay = document.getElementById("start-overlay");
   const tv = document.getElementById("tv-on");
   const startBtn = document.getElementById("start-btn");
   const startSound = document.getElementById("start-sound");
+  const backgroundMusic = document.getElementById("background-music");
 
   tv.addEventListener("animationend", () => tv.remove());
 
   startBtn.addEventListener("click", () => {
+    startSound.volume = 0.1;
     startSound.currentTime = 0;
     startSound.play();
+
+    backgroundMusic.volume = 0.05;
+    backgroundMusic.play();
+    
     overlay.style.transition = "opacity .8s";
     overlay.style.opacity = "0";
     setTimeout(() => overlay.remove(), 800);
